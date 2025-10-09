10c343fcf8c94476916a4f5ce366cbb683bec6ad
Lucarye <45335928+Lucarye@users.noreply.github.com>
2025-10-09 11:07:18 +0200

    Update triggerpost.py

--- triggerpost/triggerpost.py
+++ triggerpost/triggerpost.py
@@ -20,7 +20,7 @@ class TriggerPost(commands.Cog):
 
     def __init__(self, bot: Red):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=0xAUS_HILFE_2025, force_registration=True)
+        self.config = Config.get_conf(self, identifier=0xA1B5F00D, force_registration=True)
         self.config.register_guild(**DEFAULT_GUILD)
         self._cooldown_until = {}  # channel_id -> timestamp
 
@@ -197,3 +197,4 @@ class TriggerPost(commands.Cog):
         # Embed bauen & posten/aktualisieren
         embed = await self._build_embed(guild, message.author)
         await self._post_or_edit(message.channel, embed, data["message_id"])
+
