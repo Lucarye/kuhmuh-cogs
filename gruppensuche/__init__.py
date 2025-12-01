0d1dcbd6a2300e0ff9aba191d997be2112425869
Lucarye <45335928+Lucarye@users.noreply.github.com>
2025-12-01 13:55:59 +0100

    Update __init__.py

--- gruppensuche/__init__.py
+++ gruppensuche/__init__.py
@@ -1,6 +1,7 @@
 # __init__.py
+from redbot.core.bot import Red
 from .GruppensucheModule import Gruppensuche
 
 
-async def setup(bot):
+async def setup(bot: Red) -> None:
     await bot.add_cog(Gruppensuche(bot))
