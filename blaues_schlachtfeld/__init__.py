688cd6e85ab53bc32b9e8ee465c0e0238bff368e
Lucarye <45335928+Lucarye@users.noreply.github.com>
2026-09-12 12:37:56 +0200

    cleanup

--- blaues_schlachtfeld/__init__.py
+++
@@ -1,6 +0,0 @@
-from redbot.core.bot import Red
-from .blaues_schlachtfeld import BlauesSchlachtfeldCog
-
-
-async def setup(bot: Red) -> None:
-    await bot.add_cog(BlauesSchlachtfeldCog(bot))
--- muhinfo/__init__.py
+++
@@ -1,4 +0,0 @@
-from .muhinfo import MuhInfoCog
-
-async def setup(bot):
-    await bot.add_cog(MuhInfoCog(bot))
