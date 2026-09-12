c648e08055b9eadf8fd0435a3bdb654169198f39
Lucarye <45335928+Lucarye@users.noreply.github.com>
2026-09-12 21:17:43 +0200

    restore

--- kuhmuhupdate/__init__.py
+++ kuhmuhupdate/__init__.py
@@ -1,5 +1,5 @@
-from redbot.core.bot import Red # pyright: ignore[reportMissingImports]
-from redbot.core.utils import get_end_user_data_statement_or_raise # pyright: ignore[reportMissingImports]
+from redbot.core.bot import Red
+from redbot.core.utils import get_end_user_data_statement_or_raise
 
 from .kuhmuhupdate import KuhmuhUpdate
 
@@ -8,4 +8,3 @@ __red_end_user_data_statement__ = get_end_user_data_statement_or_raise(__file__)
 
 async def setup(bot: Red) -> None:
     await bot.add_cog(KuhmuhUpdate(bot))
-
